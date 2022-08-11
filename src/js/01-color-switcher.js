dd1d459ff86d5dc723154d98eb8460fc9dc1cd7c
Maxxbay <maxxbay@gmail.com>
2022-08-11 14:27:54 +0200

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStart = document.querySelector('.data-start');
+const btnEnd = document.querySelector('.data-stop');
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+
+btnStart.addEventListener('click', () => {
+  btnStart.disabled = true;
+  btnEnd.disabled = false;
+
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+btnEnd.addEventListener('click', () => {
+  btnStart.disabled = false;
+  btnEnd.disabled = true;
+  clearInterval(timerId);
+});
