7023419c6e48aa328b92abc6603d68d93759b28f
Maxxbay <maxxbay@gmail.com>
2022-08-11 16:23:50 +0200

    3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,21 @@
-const btnStart = document.querySelector('.data-start');
-const btnEnd = document.querySelector('.data-stop');
+const btnStart = document.querySelector('[data-start]');
+const btnEnd = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
+btnEnd.disabled = true;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 let timerId = null;
+// https://flaviocopes.com/how-to-disable-button-javascript/
 
 btnStart.addEventListener('click', () => {
   btnStart.disabled = true;
   btnEnd.disabled = false;
 
   timerId = setInterval(() => {
-    document.body.style.background = getRandomHexColor();
+    body.style.background = getRandomHexColor();
   }, 1000);
 });
 
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,1 +1,59 @@
-console.log("Hello world!");
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+
+
+const input = document.querySelector('#datetime-picker');
+const selectTimer = document.querySelector('.timer');
+const btnStart = document.querySelector('button[data-start]');
+const fields = Array.from(document.querySelectorAll('div.field'));
+const seconds = document.querySelector('span[data-seconds]');
+const minutes = document.querySelector('span[data-minutes]');
+const hours = document.querySelector('span[data-hours]');
+const days = document.querySelector('span[data-days]');
+
+let timerId = null;
+let selectedDay;
+
+startButton.disabled = true;
+
+
+startButton.addEventListener('click', countdown);
+
+// Flatpickr
+
+flatpickr(input, options);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+// Розрахунок дат, годин і тд
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
